3cadc3d5224387a506bfbbef31c9f7892a559979
Anest <efremenko.ira.rzn@gmail.com>
2024-09-15 01:35:19 +0300

    Work on mistakes

--- src/test/java/test/CardDeliveryTest.java
+++ src/test/java/test/CardDeliveryTest.java
@@ -1,10 +1,10 @@
-package ru.netology.delivery.test;
+package test;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import ru.netology.delivery.data.DataGenerator;
+//import ru.netology.delivery.data.DataGenerator;
 
 import java.time.Duration;
 
@@ -13,10 +13,11 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-class DeliveryTest {
+class CardDeliveryTest {
 
     @BeforeEach
-    void setup() {open("http://localhost:9999");
+    void setup() {
+        open("http://localhost:9999");
     }
 
     @Test
@@ -45,13 +46,10 @@ class DeliveryTest {
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
 
         $("[data-test-id=date] input").setValue(secondMeetingDate);
-        //$("[data-test-id=name] input").setValue(validUser.getName());
-       // $("[data-test-id=phone] input").setValue(validUser.getPhone());
-        //$("[data-test-id=agreement]").click();
-        //$("button.button").click();
+
 
         $(byText("Запланировать")).click();
-        //$(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+
         $("[data-test-id='replan-notification'] .notification__content")
                 .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
                 .shouldBe(visible);
@@ -66,10 +64,6 @@ class DeliveryTest {
 
 
 
-        // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
-        // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-        // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-        // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-        // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
+
 
 
